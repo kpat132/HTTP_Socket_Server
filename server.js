07a4199b2fccb5b9956ae3495dc435ba4bb8f56b
KrishnaP <kpat132@gmail.com>
2018-01-24 14:02:06 -1000

    commit1

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-net = require('net');
+const net = require('net');
 
 //Keep track of clients
 let clients = [];
 
 //Start TCP Server
-net.createServer(function(socket){
+net.server(function(socket){
 
+  clients.push(socket);
+  socket.write("Connected");
 
   
+});
+server.listen(8080,'0.0.0.0', function(){
+  console.log('Server on.')
 });
